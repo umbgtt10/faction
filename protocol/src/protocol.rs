0ce19b65bb2eca3beffd35c41e70bf2e6e42e864
Umberto Gotti <umberto.gotti@umbertogotti.dev>
2026-05-01 13:28:20 +0200

    Extract to_command() from decide() in Protocol

--- protocol/src/protocol.rs
+++ protocol/src/protocol.rs
@@ -25,18 +25,18 @@ pub struct Protocol {
 }
 
 impl Protocol {
-    pub fn cluster_view(&mut self) -> ClusterView {
-        match self.faction.process(Command::Probe) {
-            ProcessResult::Probed { cluster_view, .. } => cluster_view,
-            _ => unreachable!(),
-        }
-    }
-
     pub fn new(faction: Faction, peers: Vec<PeerId>, local_peer_id: PeerId) -> Self {
         Self {
             faction,
             peers,
             local_peer_id,
+        }
+    }
+
+    pub fn cluster_view(&mut self) -> ClusterView {
+        match self.faction.process(Command::Probe) {
+            ProcessResult::Probed { cluster_view, .. } => cluster_view,
+            _ => unreachable!(),
         }
     }
 
@@ -59,7 +59,31 @@ impl Protocol {
     }
 
     pub fn decide(&mut self, message: InputMessage) -> Vec<OutputMessage> {
-        let command = match message {
+        let command = self.to_command(message);
+
+        let outcomes = match self.faction.process(command) {
+            ProcessResult::Accepted { outcomes, .. } => outcomes,
+            ProcessResult::Probed { .. } => unreachable!(),
+            ProcessResult::Rejected { .. } => return vec![OutputMessage::Noop],
+        };
+
+        for outcome in outcomes {
+            match outcome {
+                Outcome::BroadcastLocalReady => return vec![OutputMessage::BroadcastReady],
+                Outcome::Exited { .. } => {
+                    return vec![OutputMessage::Cancel(TimerEvent::Fire(
+                        TimerMessage::LocalParticipationCompleted,
+                    ))];
+                }
+                _ => {}
+            }
+        }
+
+        vec![OutputMessage::Noop]
+    }
+
+    fn to_command(&self, message: InputMessage) -> Command {
+        match message {
             InputMessage::Transport(msg) => match msg {
                 TransportMessage::Ping { from } => Command::ParticipationObserved {
                     peer_id: from,
@@ -82,26 +106,6 @@ impl Protocol {
                 TimerMessage::LocalParticipationCompleted => Command::LocalParticipationCompleted,
                 TimerMessage::DeadlineExpired => Command::DeadlineExpired,
             },
-        };
-
-        let outcomes = match self.faction.process(command) {
-            ProcessResult::Accepted { outcomes, .. } => outcomes,
-            ProcessResult::Probed { .. } => unreachable!(),
-            ProcessResult::Rejected { .. } => return vec![OutputMessage::Noop],
-        };
-
-        for outcome in outcomes {
-            match outcome {
-                Outcome::BroadcastLocalReady => return vec![OutputMessage::BroadcastReady],
-                Outcome::Exited { .. } => {
-                    return vec![OutputMessage::Cancel(TimerEvent::Fire(
-                        TimerMessage::LocalParticipationCompleted,
-                    ))];
-                }
-                _ => {}
-            }
         }
-
-        vec![OutputMessage::Noop]
     }
 }
